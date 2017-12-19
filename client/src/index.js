31c4041f11a5dd421c901904a8d791289f7739cb
chabab hassan <chababhassan@gmail.com>
2017-12-19 23:42:08 +0000

    changed font

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
 import throttle from 'lodash/throttle';
+import WebFont from 'webfontloader';
 import Routes from "./routes/routes";
 import configureStore from "./store/configureStore";
 import {loadState, saveState} from "./utils/localStorage";
 import './main.css';
 global.Promise = require("bluebird");
 Promise.config({ cancellation: true });
-
+WebFont.load({
+  google: {
+    families: [
+      'Source Sans Pro:400,600',
+      'Quicksand:300,400,500,700'
+    ]
+  }
+});
 const persistedState = loadState();
 const store = configureStore(persistedState);
 
